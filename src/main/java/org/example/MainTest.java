b6af5a1b9a45d34dab2697ab29508be4c6945684
GatoMiau03 <d.benaldo03@ufromail.cl>
2023-09-28 11:02:30 -0300

    Pruebas unitarias

--- src/main/java/org/example/MainTest.java
+++ src/main/java/org/example/MainTest.java
@@ -1,5 +1,4 @@
 package org.example;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,82 +6,102 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
-
+import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 public class MainTest {
-    private static final int NUM_HABITACIONES = 10;
-    private final InputStream originalInput = System.in;
-    private final PrintStream originalOutput = System.out;
+    private Main hotel;
+    private InputStream originalInput;
+    private PrintStream originalOutput;
     private ByteArrayOutputStream outputStream;
 
     @BeforeEach
     public void setUp() {
-
-        for (int i = 0; i < NUM_HABITACIONES; i++) {
-            Main.estadoHabitaciones[i] = null;
-        }
+        originalInput = System.in;
+        originalOutput = System.out;
         outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
     }
 
     @AfterEach
     public void restoreStreams() {
-        // Restaurar la entrada y salida estándar originales
         System.setIn(originalInput);
         System.setOut(originalOutput);
     }
 
+    @Test
+    public void consultarEstadoHabitaciones() throws NoSuchFieldException, IllegalAccessException {
+        hotel = new Main(3);
+        String[] estadoHabitaciones = new String[3];
+        estadoHabitaciones[0] = "Ocupada con alimentación";
+        estadoHabitaciones[1] = "Ocupada sin alimentación";
+        estadoHabitaciones[2] = "Reservada";
+        setEstadoHabitaciones(hotel, estadoHabitaciones);
+
+        hotel.consultarEstadoHabitaciones();
+
+        String expectedOutput = "Estado de las habitaciones:\n" +
+                "Habitación 1: Ocupada con alimentación\n" +
+                "Habitación 2: Ocupada sin alimentación\n" +
+                "Habitación 3: Reservada\n";
+        assertEquals(expectedOutput, outputStream.toString());
+    }
+
 
     @Test
-    public void consultarEstadoHabitaciones() {
-        Main.estadoHabitaciones[0] = "Ocupada con alimentación";
-        Main.estadoHabitaciones[1] = "Ocupada sin alimentación";
-        Main.estadoHabitaciones[2] = "Reservada";
+    public void obtenerEstadoHabitacionLibreTest() throws NoSuchFieldException, IllegalAccessException {
+        hotel = new Main(3);
+        String[] estadoHabitaciones = new String[3];
+        setEstadoHabitaciones(hotel, estadoHabitaciones);
 
-        String resultado = Main.obtenerEstadoHabitacion(0);
-
-        assertEquals("Ocupada con alimentación", resultado);
+        String estado = hotel.obtenerEstadoHabitacion(2);
+        assertEquals("Habitación liberada", estado);
     }
 
     @Test
-    public void obtenerEstadoHabitacionLibreTest() {
-        String estado = Main.obtenerEstadoHabitacion(2);
-
-        assertEquals("Habitacion liberada", estado);
-    }
-    @Test
-    public void reservarHabitacionTest() {
+    public void reservarHabitacionTest() throws NoSuchFieldException, IllegalAccessException {
+        hotel = new Main(3);
         String input = "3\nn\n";
         ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
         System.setIn(inputStream);
 
-
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
-
-        Main.reservarHabitacion();
+        hotel.reservarHabitacion();
 
         String systemOutput = outputStream.toString();
 
         assertTrue(systemOutput.contains("Habitación 3 reservada con éxito."));
-        assertEquals("Ocupada sin alimentación", Main.estadoHabitaciones[2]);
+
+        Field estadoHabitacionesField = hotel.getClass().getDeclaredField("estadoHabitaciones");
+        estadoHabitacionesField.setAccessible(true);
+        String[] estadoHabitaciones = (String[]) estadoHabitacionesField.get(hotel);
+        assertEquals("Ocupada sin alimentación", estadoHabitaciones[2]);
     }
 
     @Test
-    public void liberarHabitacionTest() {
-        // Configurar una habitación previamente ocupada y luego liberarla
-        Main.estadoHabitaciones[1] = "Ocupada sin alimentación";
+    public void liberarHabitacionTest() throws NoSuchFieldException, IllegalAccessException {
+        hotel = new Main(3);
+        String[] estadoHabitaciones = new String[3];
+        estadoHabitaciones[1] = "Ocupada sin alimentación";
+        setEstadoHabitaciones(hotel, estadoHabitaciones);
 
-        // Comprobar que la habitación está ocupada antes de liberarla
-        assertEquals("Ocupada sin alimentación", Main.estadoHabitaciones[1]);
+        assertEquals("Ocupada sin alimentación", estadoHabitaciones[1]);
 
-        // Ejecutar el método de liberación de habitación
-        Main.liberarHabitacion(); // Liberar la habitación actual
+        String input = "2\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
 
-        // Comprobar que la habitación se ha liberado correctamente
-        assertNull(Main.estadoHabitaciones[1]);
+        hotel.liberarHabitacion();
+
+        Field estadoHabitacionesField = hotel.getClass().getDeclaredField("estadoHabitaciones");
+        estadoHabitacionesField.setAccessible(true);
+        estadoHabitaciones = (String[]) estadoHabitacionesField.get(hotel);
+        assertNull(estadoHabitaciones[1]);
+    }
+
+    private void setEstadoHabitaciones(Main main, String[] estadoHabitaciones) throws NoSuchFieldException, IllegalAccessException {
+        Field estadoHabitacionesField = main.getClass().getDeclaredField("estadoHabitaciones");
+        estadoHabitacionesField.setAccessible(true);
+        estadoHabitacionesField.set(main, estadoHabitaciones);
     }
 }
