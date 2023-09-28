4184641054b27c948981845a50614f02ef309ffc
Alex Sáez <135442605+Avzix@users.noreply.github.com>
2023-09-28 10:34:51 -0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,121 @@
-{
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        final int NUM_HABITACIONES = 10;
+
+
+        Habitacion[] habitaciones = new Habitacion[NUM_HABITACIONES];
+
+        for (int i = 0; i < NUM_HABITACIONES; i++) {
+            habitaciones[i] = new Habitacion();
+        }
+
+        while (true) {
+            mostrarMenu();
+            int opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    consultarEstadoHabitaciones(habitaciones);
+                    break;
+                case 2:
+                    reservarHabitacion(scanner, habitaciones);
+                    break;
+                case 3:
+                    liberarHabitacion(scanner, habitaciones);
+                    break;
+                case 4:
+                    ingresarCantidadNoches(scanner, habitaciones);
+                    break;
+                case 5:
+                    confirmarReserva(scanner, habitaciones);
+                    break;
+                case 6:
+                    System.out.println("Gracias por usar el sistema de administración del hotel.");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Opción no válida. Intente nuevamente.");
+            }
+        }
+    }
+
+    private static void mostrarMenu() {
+        System.out.println("\nMenú:");
+        System.out.println("1. Consultar estado de habitaciones");
+        System.out.println("2. Reservar habitación");
+        System.out.println("3. Liberar habitación");
+        System.out.println("4. Ingresar cantidad de noches");
+        System.out.println("5. Confirmar reserva");
+        System.out.println("6. Salir");
+        System.out.print("Seleccione una opción: ");
+    }
+
+    private static void consultarEstadoHabitaciones(Habitacion[] habitaciones) {
+        final int NUM_HABITACIONES = 10;
+        System.out.println("\nEstado de las habitaciones:");
+        for (int i = 0; i < NUM_HABITACIONES; i++) {
+            System.out.println("Habitación " + (i + 1) + ": " + habitaciones[i].obtenerEstado());
+        }
+    }
+
+    private static void reservarHabitacion(Scanner scanner, Habitacion[] habitaciones) {
+        final int NUM_HABITACIONES = 10;
+
+        System.out.print("Ingrese el número de habitación a reservar: ");
+        int numeroHabitacion = scanner.nextInt() - 1;
+        if (numeroHabitacion >= 0 && numeroHabitacion < NUM_HABITACIONES) {
+            habitaciones[numeroHabitacion].reservar(scanner);
+        } else {
+            System.out.println("Número de habitación no válido.");
+        }
+    }
+
+    private static void liberarHabitacion(Scanner scanner, Habitacion[] habitaciones) {
+        final int NUM_HABITACIONES = 10;
+
+        System.out.print("Ingrese el número de habitación a liberar: ");
+        int numeroHabitacion = scanner.nextInt() - 1;
+        if (numeroHabitacion >= 0 && numeroHabitacion < NUM_HABITACIONES) {
+            habitaciones[numeroHabitacion].liberar();
+        } else {
+            System.out.println("Número de habitación no válido.");
+        }
+    }
+
+    private static void ingresarCantidadNoches(Scanner scanner, Habitacion[] habitaciones) {
+        final int NUM_HABITACIONES = 10;
+
+        System.out.print("Ingrese el número de habitación para ingresar la cantidad de noches: ");
+        int numeroHabitacion = scanner.nextInt() - 1;
+        if (numeroHabitacion >= 0 && numeroHabitacion < NUM_HABITACIONES) {
+            habitaciones[numeroHabitacion].ingresarNoches(scanner);
+        } else {
+            System.out.println("Número de habitación no válido.");
+        }
+    }
+
+    private static void confirmarReserva(Scanner scanner, Habitacion[] habitaciones) {
+        final int NUM_HABITACIONES = 10;
+
+        System.out.print("Ingrese el número de habitación para confirmar la reserva: ");
+        int numeroHabitacion = scanner.nextInt() - 1;
+        if (numeroHabitacion >= 0 && numeroHabitacion < NUM_HABITACIONES) {
+            habitaciones[numeroHabitacion].confirmarReserva();
+        } else {
+            System.out.println("Número de habitación no válido.");
+        }
+    }
+}
+
+class Habitacion {
     private String estado;
     private int nochesReservadas;
 
